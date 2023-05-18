c8258d69e022f21768c522c247ad9398640e1163
Mikołaj Lubiak <lubiak@proton.me>
2023-05-18 16:10:33 +0200

    Finally working version

--- main.py
+++ main.py
@@ -1,24 +1,35 @@
 import discord
+from discord.ext.commands import Bot
 
 intents = discord.Intents.default()
 intents.message_content = True
 
-client = discord.Client(intents=intents)
+client = Bot(command_prefix='', intents=intents)
+
+@client.command()
+async def aha(ctx):
+        await ctx.message.delete()
+        webhook = await ctx.channel.create_webhook(name=ctx.author.nick)
+        await webhook.send(
+            'https://cdn.7tv.app/emote/6287c2ca6d9cd2d1f31b5e7d/4x.gif', username=ctx.author.name, avatar_url=ctx.author.avatar)
+
+        webhooks = await ctx.channel.webhooks()
+        for webhook in webhooks:
+                await webhook.delete()
+
+@client.command()
+async def xdd(ctx):
+        await ctx.message.delete()
+        webhook = await ctx.channel.create_webhook(name=ctx.author.nick)
+        await webhook.send(
+            'https://cdn.7tv.app/emote/613937fcf7977b64f644c0d2/3x.webp', username=ctx.author.name, avatar_url=ctx.author.avatar)
+
+        webhooks = await ctx.channel.webhooks()
+        for webhook in webhooks:
+                await webhook.delete()
 
 @client.event
 async def on_ready():
     print(f'We have logged in as {client.user}')
-
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
-
-    if message.content.startswith('xdd'):
-        await message.channel.send('https://cdn.7tv.app/emote/613937fcf7977b64f644c0d2/4x.webp')
-        await message.delete()
-    if message.content.startswith('aha'):
-        await message.channel.send('https://cdn.7tv.app/emote/6287c2ca6d9cd2d1f31b5e7d/4x.gif')
-        await message.delete()
 
 client.run('TOKEN')
