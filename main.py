185ed90972924b10a0a0745cfa7cfc0ba75fd115
Mikołaj Lubiak <lubiak@proton.me>
2023-05-18 16:27:38 +0200

    added nerd

--- main.py
+++ main.py
@@ -28,6 +28,17 @@ async def xdd(ctx):
         for webhook in webhooks:
                 await webhook.delete()
 
+@client.command()
+async def nerd(ctx):
+        await ctx.message.delete()
+        webhook = await ctx.channel.create_webhook(name=ctx.author.nick)
+        await webhook.send(
+            'https://cdn.7tv.app/emote/6134bc74f67d73ea27e44b0f/4x.gif', username=ctx.author.name, avatar_url=ctx.author.avatar)
+
+        webhooks = await ctx.channel.webhooks()
+        for webhook in webhooks:
+                await webhook.delete()
+
 @client.event
 async def on_ready():
     print(f'We have logged in as {client.user}')
