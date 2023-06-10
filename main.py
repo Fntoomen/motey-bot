0c5eaa53af2c96b8dc056fd0628a6fe55ed9fd51
Mikołaj Lubiak <lubiak@proton.me>
2023-06-10 20:12:45 +0200

    Set the working directory as the file directory

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
 import discord
 import random
 import mariadb
+import os
+
+abspath = os.path.abspath(__file__)
+dname = os.path.dirname(abspath)
+os.chdir(dname)
 
 conn = mariadb.connect (
         host="localhost",
         user="root",
         password="passwd",
-        database="emotebot"
+        database="emote"
         )
 cur = conn.cursor()
 
